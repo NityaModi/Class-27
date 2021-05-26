9079d7d23721380644315774d681cf7a86badf12
NityaModi <78858832+NityaModi@users.noreply.github.com>
2021-05-26 17:01:58 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,19 @@
+class SlingShot{
+constructor(bodyA,bodyB){
+    var options = {
+        bodyA:bodyA,
+        bodyB:bodyB,
+        stiffness:0.04,
+        length:10
+    }
+    this.Sling=Constraint.create(options);
+World.add(world,this.Sling);
+}
+display(){
+    var pointA=this.Sling.bodyA.position;
+    var pointB=this.Sling.bodyB.position;
+    strokeWeight(3);
+    line (pointA.x,pointA.y,pointB.x,pointB.y);
+}
+
+}
